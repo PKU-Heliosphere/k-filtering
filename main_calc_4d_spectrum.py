7be119d1716af607916f71fdbafaa8fec73687e1
Linronald874 <1600012432@pku.edu.cn>
2020-01-31 20:57:47 +0800

    update current main subscripts.

--- main_calc_4d_spectrum.py
+++ main_calc_4d_spectrum.py
@@ -28,33 +28,44 @@ def calc_spectrum_4d():
     The shape of the array: num of omega * num of kx * num of ky * num of kz.
     :return: None.
     """
-    # t_s, s_data = input_data_E_field()
-    s_data = np.load('interped_E_data.npy')
-    M_mat_list = build_M_matrices_list(s_data)[:128]
+    from parameters import dt, LEN_FFT, NUM_OF_OMEGAS, BIG_NUM_OMEGAS
+
+    # BIG_NUM_OMEGAS = NUM_OF_OMEGAS
+    # BIG_NUM_OMEGAS is used to calculate P with many omegas so that one can then calculate P with omega_pl as the
+    # independent variable.
+    # if one want to use fewer omegas, just dis-comment 'BIG_NUM_OMEGAS = NUM_OF_OMEGAS'.
+
+    s_data = np.load('Eprime2_brst_data.npy')  # change this line to change the input file.
+    save_name = 'big_omega_spectrum_4d_Eprime2'  # change this line to change the name of the saved file.
+
+    M_mat_list = build_M_matrices_list(s_data)[:BIG_NUM_OMEGAS]
 
     k_list, kx_list, ky_list, kz_list = get_k_list()
 
     spectrum_4d = list()
 
-    omega_list = 2 * np.pi * np.fft.rfftfreq(1024, 0.0625)[:128]
+    omega_list = 2 * np.pi * np.fft.rfftfreq(LEN_FFT, dt)[:BIG_NUM_OMEGAS]
 
+    count = 1
     for omega, M_this_omega in zip(omega_list, M_mat_list):
-        result = np.zeros((n_k, n_k+1, n_k-1), dtype=np.double)
+        result = np.zeros((n_k, n_k + 1, n_k - 1), dtype=np.double)
         n = 0
         start = time.time()
 
         for i in range(n_k):
-            for j in range(n_k+1):
-                for l in range(n_k-1):
+            for j in range(n_k + 1):
+                for l in range(n_k - 1):
                     result[i, j, l] = np.real(P_value(H_matrix(k_list[n]), M_this_omega))  # calculate P
                     n = n + 1
 
         end = time.time()
-        print('n k total = ', n, 'it takes ', end - start, 'second(s) to calculate using this omega:', omega)
+        print('n k total =', n, ', it takes', end - start, 'second(s) to calculate using this omega:',
+              omega, '(' + str(count) + '/' + str(BIG_NUM_OMEGAS) + ')')
         spectrum_4d.append(result)
+        count += 1
 
     spectrum_4d_array = np.array(spectrum_4d)
-    np.save('spectrum_4d', spectrum_4d_array)
+    np.save(save_name, spectrum_4d_array)
 
 
 if __name__ == '__main__':
--- main_plot_data.py
+++ main_plot_data.py
@@ -2,6 +2,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from location_and_klist import get_k_list
 
+kx_list, ky_list, kz_list = get_k_list()[1:]
+
 
 def plot_ki_kj_plane_accumulated(ax, spectrum_4d, ki='kx', kj='ky'):
     accumulated_spec = spectrum_4d.sum(axis=0)  # add P values of different omegas.
@@ -9,10 +11,18 @@ def plot_ki_kj_plane_accumulated(ax, spectrum_4d, ki='kx', kj='ky'):
     spec_for_plot = accumulated_spec.sum(axis=summing_axis)
 
     k_list_dic = {'kx': kx_list, 'ky': ky_list, 'kz': kz_list}
-    ax.pcolor(k_list_dic[kj], k_list_dic[ki], spec_for_plot)  # the ki_list is the same
-    ax.set_xlabel(kj + '(km^-1)')
-    ax.set_ylabel(ki + '(km^-1)')
-    ax.set_title('P(' + ki + ',' + kj + '),omega accumulated')
+    k_i, k_j = ki[0] + '_' + ki[1], kj[0] + '_' + kj[1]
+
+    DO_LOG = False
+    if DO_LOG:
+        contour = ax.contourf(k_list_dic[kj], k_list_dic[ki], np.log(spec_for_plot))
+        ax.set_title('$\\log{P}(' + k_i + ',' + k_j + ')$', fontsize=15)
+    else:
+        contour = ax.contourf(k_list_dic[kj], k_list_dic[ki], spec_for_plot)  # the ki_list is the same
+        ax.set_title('${P}(' + k_i + ',' + k_j + ')$', fontsize=15)
+    ax.set_xlabel('$' + k_j + '\\rm{(km^{-1})}$', fontsize=13)
+    ax.set_ylabel('$' + k_i + '\\rm{(km^{-1})}$', fontsize=13)
+    return contour
 
 
 def plot_ki_kj_plane_single_omega(ax, spectrum_one_omega, omega, ki='kx', kj='ky'):
@@ -20,74 +30,151 @@ def plot_ki_kj_plane_single_omega(ax, spectrum_one_omega, omega, ki='kx', kj='ky
     spec_for_plot = spectrum_one_omega.sum(axis=summing_axis)
 
     k_list_dic = {'kx': kx_list, 'ky': ky_list, 'kz': kz_list}
-    ax.pcolor(k_list_dic[kj], k_list_dic[ki], spec_for_plot)  # the ki_list is the same
+    pcolor_obj = ax.contourf(k_list_dic[kj], k_list_dic[ki], spec_for_plot)  # the ki_list is the same
     ax.set_xlabel(kj + '(km^-1)')
     ax.set_ylabel(ki + '(km^-1)')
-    ax.set_title('P(' + ki + ',' + kj + '),omega =' + str(omega)[:5])
+    ax.set_title(
+        'P_{E\'}/P{E}(' + ki + ',' + kj + ') \nat omega=' + str(omega)[:5] +
+        'rad/s\n with integral at the other k_i' +
+        ' direction')
+    # \' = E + V cross B
+    plt.colorbar(pcolor_obj, ax=ax)
 
 
-def plot_omega_ki_plane(ax, spectrum_4d, ki='kx'):
+def plot_omega_ki_plane_accumulated(ax, spectrum_4d, ki='kx'):
     summing_axis = {'kx': (3, 2), 'ky': (3, 1), 'kz': (1, 1)}[ki]
     spec_for_plot = spectrum_4d.sum(axis=summing_axis[0])
     spec_for_plot = spec_for_plot.sum(axis=summing_axis[1])
 
     k_list_dic = {'kx': kx_list, 'ky': ky_list, 'kz': kz_list}
-    ax.pcolor(k_list_dic[ki], omega_list, spec_for_plot)
-    ax.set_ylabel('omega(rad/s)')
-    ax.set_xlabel(ki + '(km^-1)')
-    ax.set_title('P(omega,' + ki + ')')
+    k_i = ki[0] + '_' + ki[1]
+    DO_LOG = False
+    if DO_LOG:
+        contour = ax.contourf(k_list_dic[ki], omega_list, np.log(spec_for_plot))
+        ax.set_title('$\\log{P}(\\omega ,' + k_i + ')$', fontsize=15)
+    else:
+        contour = ax.contourf(k_list_dic[ki], omega_list, spec_for_plot)
+        ax.set_title('${P}(\\omega ,' + k_i + ')$', fontsize=15)
+    ax.set_ylabel('$\\omega \\rm{(rad/s)}$', fontsize=13)
+    ax.set_xlabel('$' + k_i + '\\rm{(km^{-1})}$', fontsize=13)
+    return contour
 
 
 if __name__ == '__main__':
-    spectrum_4d = np.load('spectrum_4d.npy')
-    omega_list = 2 * np.pi * np.fft.rfftfreq(1024, 0.03125)[1:128]
-    kx_list, ky_list, kz_list = get_k_list()[1:]
+    from parameters import dt, LEN_FFT, NUM_OF_OMEGAS, BIG_NUM_OMEGAS
+
+    # NUM_OF_OMEGAS = BIG_NUM_OMEGAS
+    #
+    # spectrum_4d = np.load('modified_spectrum_4d_Eprime3.npy')
+    # omega_list = 2 * np.pi * np.fft.rfftfreq(LEN_FFT, dt)[1:NUM_OF_OMEGAS]
+    # 74 rather than 75
+
+    # spectrum_4d = np.load('spectrum_4d_B.npy')
+    # omega_list = 2 * np.pi * np.fft.rfftfreq(LEN_FFT, dt)[:NUM_OF_OMEGAS]
+
+    # spectrum_4d = np.load('spectrum_4d_E.npy')
+    # omega_list = 2 * np.pi * np.fft.rfftfreq(LEN_FFT, dt)[:NUM_OF_OMEGAS]
+
+    spectrum_4d = np.load('inplasma_spectrum_Eprime3.npy')
+    omega_list = 2 * np.pi * np.fft.rfftfreq(LEN_FFT, dt)[1:NUM_OF_OMEGAS]
     '''
-    figure, axes = plt.subplots(2, 3, figsize=(15, 10))
-    plot_ki_kj_plane_accumulated(axes[0, 0], spectrum_4d[1:128], ki='kx', kj='ky')
-    plot_ki_kj_plane_accumulated(axes[0, 1], spectrum_4d[1:128], ki='ky', kj='kz')
-    plot_ki_kj_plane_accumulated(axes[0, 2], spectrum_4d[1:128], ki='kx', kj='kz')
-
-    plot_omega_ki_plane(axes[1, 0], spectrum_4d[1:128], ki='kx')
-    plot_omega_ki_plane(axes[1, 1], spectrum_4d[1:128], ki='ky')
-    plot_omega_ki_plane(axes[1, 2], spectrum_4d[1:128], ki='kz')
-
-    # axes[1, 0].plot([0, -0.2], [0, -0.2*-102], 'k-')
-    # axes[1, 1].plot([0, -0.2], [0, -0.2*-94], 'k-')
-    # axes[1, 2].plot([0, 0.2], [0, 0.2*19], 'k-')
-    # 画出速度
-
-    
-    [-102.3152   -94.38133   18.816  ] [22.831957 18.871883 21.592825]
-    [-102.02163  -93.91957   18.82654] [23.450603 19.106739 21.603088]
-    [-102.23699   -94.21786    18.677534] [23.117826 19.140852 21.665476]
-    [-102.55533   -94.41209    18.782299] [22.32191  18.927166 21.5276  ]
-    
-
-    plt.savefig('anisotropic_accumulated_and_dispersion_0513')
-    plt.show()
+    remember: the in-plasma spectrum doesn't have omega = 0.
     '''
 
-    for i in range(63):
-        my_figure, ax = plt.subplots()
-        plot_ki_kj_plane_single_omega(ax, spectrum_4d[i], omega_list[i], ki='kx', kj='ky')
-        plt.savefig('P_kx_ky_omega_' + str(omega_list[i])[:5] + '.png')
-        plot_ki_kj_plane_single_omega(ax, spectrum_4d[i], omega_list[i], ki='ky', kj='kz')
-        plt.savefig('P_ky_kz_omega_' + str(omega_list[i])[:5] + '.png')
-        plot_ki_kj_plane_single_omega(ax, spectrum_4d[i], omega_list[i], ki='kx', kj='kz')
-        plt.savefig('P_kx_kz_omega_' + str(omega_list[i])[:5] + '.png')
-        print(i)
+    # spectrum_4d = np.load('spectrum_4d_Eprime3.npy')
+    # omega_list = 2 * np.pi * np.fft.rfftfreq(LEN_FFT, dt)[:NUM_OF_OMEGAS]
 
-    # spec = spectrum_4d.sum(axis=1)
-    # spec = spec.sum(axis=1)
-    # spec = spec.sum(axis=1)
-    # from fft_data import LEN_FFT, dt
-    # omega_list = 2 * np.pi * np.fft.rfftfreq(1024, 0.03125)[:128]
-    # plt.plot(omega_list, 2*spec ** 2/LEN_FFT*dt)
-    # plt.semilogy()
-    # plt.semilogx()
-    # plt.xlabel('frequency(Hz)')
-    # plt.ylabel('PSD(nT^2/Hz) * A CONSTANT')
-    # plt.title('PSD power law test')
-    # plt.savefig('power_law_test.png')
+    # spectrum_4d = np.load('spec_Epr_over_E_(modified).npy')
+    # omega_list = 2 * np.pi * np.fft.rfftfreq(LEN_FFT, dt)[1:NUM_OF_OMEGAS]
+
+    figure, axes = plt.subplots(2, 3, figsize=(15, 9))
+    plt.subplots_adjust(wspace=0.4, hspace=0.3)
+    a = plot_ki_kj_plane_accumulated(axes[0, 0], spectrum_4d[:NUM_OF_OMEGAS], ki='kx', kj='ky')
+    b = plot_ki_kj_plane_accumulated(axes[0, 1], spectrum_4d[:NUM_OF_OMEGAS], ki='ky', kj='kz')
+
+    c = plot_ki_kj_plane_accumulated(axes[0, 2], spectrum_4d[:NUM_OF_OMEGAS], ki='kx', kj='kz')
+
+    d = plot_omega_ki_plane_accumulated(axes[1, 0], spectrum_4d[:NUM_OF_OMEGAS], ki='kx')
+    e = plot_omega_ki_plane_accumulated(axes[1, 1], spectrum_4d[:NUM_OF_OMEGAS], ki='ky')
+    f = plot_omega_ki_plane_accumulated(axes[1, 2], spectrum_4d[:NUM_OF_OMEGAS], ki='kz')
+
+    # plt.colorbar(a, ax=axes[0, 0], format='%.1e')
+    # plt.colorbar(b, ax=axes[0, 1], format='%.1e')
+    # plt.colorbar(c, ax=axes[0, 2], format='%.1e')
+    # plt.colorbar(d, ax=axes[1, 0], format='%.1e')
+    # plt.colorbar(e, ax=axes[1, 1], format='%.1e')
+    # plt.colorbar(f, ax=axes[1, 2], format='%.1e')
+
+    # plot the B-direction
+    B_avg = [-2.7201579, 17.27789, 34.078583]
+    zoom_rate = 300
+    axes[0, 0].arrow(0, 0, B_avg[1] / zoom_rate, B_avg[0] / zoom_rate, color='r',width=0.005)
+    axes[0, 1].arrow(0, 0, B_avg[2] / zoom_rate, B_avg[1] / zoom_rate, color='r',width=0.005)
+    axes[0, 2].arrow(0, 0, B_avg[2] / zoom_rate, B_avg[0] / zoom_rate, color='r',width=0.005)
+
+    plt.savefig('images/anisotropic_accumulated_and_dispersion')
     # plt.show()
+
+    do_slices = input('input a \'y\' to start drawing slices of ki kj planes.\n')
+    if do_slices == 'y':
+        for i in range(NUM_OF_OMEGAS):
+            my_figure, axis = plt.subplots()
+            plot_ki_kj_plane_single_omega(axis, spectrum_4d[i], omega_list[i], ki='kx', kj='ky')
+            plt.savefig('images/P_kx_ky_omega_' + str(omega_list[i])[:5] + '.png')
+
+            my_figure, axis = plt.subplots()
+            plot_ki_kj_plane_single_omega(axis, spectrum_4d[i], omega_list[i], ki='ky', kj='kz')
+            plt.savefig('images/P_ky_kz_omega_' + str(omega_list[i])[:5] + '.png')
+
+            my_figure, axis = plt.subplots()
+            plot_ki_kj_plane_single_omega(axis, spectrum_4d[i], omega_list[i], ki='kx', kj='kz')
+            plt.savefig('images/P_kx_kz_omega_' + str(omega_list[i])[:5] + '.png')
+            print(i)
+    else:
+        print('don\'t receive a \'y\', now quit.')
+
+    do_psd = input('input a \'y\' to start drawing PSD.\n')
+    if do_psd == 'y':
+        spec = spectrum_4d.sum(axis=1)
+        spec = spec.sum(axis=1)
+        spec = spec.sum(axis=1)
+        from fft_data import LEN_FFT, dt
+
+        omega_list = 2 * np.pi * np.fft.rfftfreq(LEN_FFT, dt)[:NUM_OF_OMEGAS]
+
+        my_figure, axis = plt.subplots()
+        plt.plot(omega_list, 2 * spec ** 2 / LEN_FFT * dt)
+        plt.semilogy()
+        plt.semilogx()
+        plt.xlabel('omega(rad/s)')
+        plt.ylabel('PSD(nT^2/Hz) * A CONSTANT')
+        plt.title('PSD power law test')
+        plt.savefig('images/power_law_test.png')
+        # plt.show()
+    else:
+        print('don\'t receive a \'y\', now quit.')
+
+'''
+def plot_omega_kx_plane_with_kykz_fixed(ax, spectrum_4d, i_ky, i_kz):
+    spec_for_plot = spectrum_4d[:, :, i_ky, i_kz]
+    ax.pcolor(kx_list, omega_list, spec_for_plot)
+    ax.set_ylabel('omega(rad/s)')
+    ax.set_xlabel('kx(km^-1)')
+    ax.set_title('P(omega, kx), ky=' + ky_list[i_ky] + 'kz=' + kz_list[i_kz])
+
+
+def plot_omega_ky_plane_with_kxkz_fixed(ax, spectrum_4d, i_kx, i_kz):
+    spec_for_plot = spectrum_4d[:, i_kx, :, i_kz]
+    ax.pcolor(ky_list, omega_list, spec_for_plot)
+    ax.set_ylabel('omega(rad/s)')
+    ax.set_xlabel('ky(km^-1)')
+    ax.set_title('P(omega, ky), kx=' + kx_list[i_kx] + 'kz=' + kz_list[i_kz])
+
+
+def plot_omega_kz_plane_with_kxky_fixed(ax, spectrum_4d, i_kx, i_ky):
+    spec_for_plot = spectrum_4d[:, i_kx, i_ky, :]
+    ax.pcolor(kz_list, omega_list, spec_for_plot)
+    ax.set_ylabel('omega(rad/s)')
+    ax.set_xlabel('kz(km^-1)')
+    ax.set_title('P(omega, kz), kx=' + kx_list[i_kx] + 'ky=' + ky_list[i_ky])
+'''
