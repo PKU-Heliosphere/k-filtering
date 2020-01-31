a7c62dc6ca730af5b5e6cac14e43e4dbcf50fbdf
Linronald874 <1600012432@pku.edu.cn>
2020-01-31 20:56:51 +0800

    update parameters including location and k list

--- parameters.py
+++ parameters.py
@@ -1,4 +1,24 @@
-LEN_FFT = 1024
-dt = 0.0625  # second(s)
-LEN_DATA = 4096-256  # 4096 for B field
-LEN_MOVE_ONCE = 256  # 256 for B field
+LEN_FFT = 4096
+# dt = 0.007813
+# magnetic field, psr's case
+
+dt = 0.014649 / 2
+# electric field, psr's case
+
+LEN_DATA = 9200
+LEN_MOVE_ONCE = 512
+NUM_OF_OMEGAS = 75
+
+BIG_NUM_OMEGAS = 290
+# for the use of BIG_NUM_OMEGAS, see main_calc_4d_spectrum.py
+
+##################################
+# the former case.
+
+# LEN_FFT = 1024
+# dt = 0.0625  # second(s)
+# LEN_DATA = 4096-256  # 4096 for B field
+# LEN_MOVE_ONCE = 256  # 256 for B field
+# NUM_OF_OMEGAS = 73
+
+##################################
