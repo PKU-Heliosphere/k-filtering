eed0338604eb6a781b5f1143a7a8b81bbac781ce
Linronald874 <1600012432@pku.edu.cn>
2020-08-07 18:30:49 +0800

    add MUSIC and MSR and my method.

--- filter_centrl_algrthm.py
+++ filter_centrl_algrthm.py
@@ -29,13 +29,57 @@ trace = np.trace
 
 def P_value(H_mat, M_mat):
     temp = dagger(H_mat)  # = H_dagger
-    temp = dot(temp, inv(M_mat))  # = H_dagger * M_inv
+    try:
+        temp = dot(temp, inv(M_mat))  # = H_dagger * M_inv
+    except Exception as e:
+        print(e)
+        return 0
     temp = dot(temp, H_mat)  # = H_dagger * M_inv * H
     try:
-        temp = inv(temp) # = (H_dagger * M_inv * H)_inv
+        temp = inv(temp)  # = (H_dagger * M_inv * H)_inv
         return trace(temp)
-    except:
-        print(temp)
+    except Exception as e:
+        print(e)
+        return 0
+
+
+def my_sigmoid(x):
+    t = (x - 1 / 2) * 10
+    return 1 / (1 + np.exp(-t)) + 1e-6
+
+
+def P_value_MUSIC(H_mat, M_mat, cut_level=0, power_level=0, automode=0):
+    temp = dagger(H_mat)
+    try:
+        M_eigenvalues, M_feature_vectors = np.linalg.eig(M_mat)
+        lambda_max = np.max(np.abs(M_eigenvalues))
+        normalized_lambdas = M_eigenvalues / lambda_max
+        if cut_level:
+            # the MUSIC method (cut certain eigenvalues), see (Narita, 2010. Wave vector...)
+            normalized_lambdas[:cut_level] = 1
+            normalized_lambdas[cut_level:] = 1e-5
+        elif power_level:
+            # the extended MUSIC method (use a power function), see (Narita, 2010. Wave vector...)
+            normalized_lambdas *= np.abs(normalized_lambdas) ** (power_level - 1)
+        elif automode:
+            # my method, replace the step function in MUSIC method with a function like sigmoid
+            # (in a sense) auto-adaptive
+            normalized_lambdas = (1 - my_sigmoid(1 / normalized_lambdas) + 1e-6) * 100
+        else:
+            print('No mode received, return 0.')
+            return 0
+        pseudo_M_inv = dot(dot(M_feature_vectors, np.diag(1 / (normalized_lambdas))),
+                           M_feature_vectors.T.conj())
+        temp = dot(temp, pseudo_M_inv)
+    except Exception as e:
+        print(e)
+        return 0
+    temp = dot(temp, H_mat)  # = H_dagger * M_inv * H
+    try:
+        temp = inv(temp)  # = (H_dagger * M_inv * H)_inv
+        return trace(temp)
+    except Exception as e:
+        print(e)
         return 0
 
 
@@ -49,11 +93,13 @@ if __name__ == '__main__':
     from M_matrices import build_M_matrices_list
 
     M_case = build_M_matrices_list(s_data)[10]
+    print(M_case.shape)
 
     import time
 
     start = time.time()
     print('P(k=(0.2,0.2,0.2))', P_value(H, M_case))
+    print('P(k=(0.2,0.2,0.2))', P_value_MUSIC(H, M_case))
     k_vector = (-0.2, 0.2, 0.2)
     H = H_matrix(k_vector)
     print('P(k=(-0.2,0.2,0.2))', P_value(H, M_case))
